08730554ce9cb5f3d8b73e964e3c4d8ddfb887ef
Heliovic <1005031096@qq.com>
2019-05-30 11:10:28 +0800

    2018-01

---
+++ 2018-01/2018-01.cpp
@@ -0,0 +1,38 @@
+﻿// 2018-01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+/*
+输入只有一行，包含两个整数a,b，要求输出[a,b]内所有的回文数，a,b<10^6
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int A, B;
+
+int main()
+{
+	scanf("%d %d", &A, &B);
+
+	char buf[128];
+
+	for (int i = A; i <= B; i++)
+	{
+		sprintf(buf, "%d", i);
+		int len = strlen(buf);
+
+		bool flag = true;
+
+		for (int i = 0; i <= len / 2 && flag; i++)
+			if (buf[i] != buf[len - 1 - i])
+				flag = false;
+		if (flag)
+			printf("%d\n", i);
+	}
+
+	return 0;
+}
